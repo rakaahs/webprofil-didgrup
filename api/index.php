fc447de54eb73a91fe901f8a655cb30681e2c834
rakaahs <rakasyahrafi@gmail.com>
2026-05-25 18:56:46 +0700

    Configure Laravel for Vercel Serverless read-only filesystem

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
 <?php
 
+// Konfigurasi untuk Vercel Serverless (Read-only filesystem)
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+$_SERVER['SESSION_DRIVER'] = 'cookie';
+$_SERVER['LOG_CHANNEL'] = 'stderr';
+$_SERVER['APP_ENV'] = 'production';
+
 // Forward Vercel requests to normal Laravel index.php
 require __DIR__ . '/../public/index.php';
